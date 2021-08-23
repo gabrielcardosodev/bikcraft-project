dfcbfea5ba00ccc01f03fef1764492ee520ee0db
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2021-08-23 17:27:58 -0300

    Add simple-slide in portfolio page

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
+
+  new SimpleSlide({
+    slide: 'portfolio',
+    time: 5000,
+    pauseOnHover: true,
+    nav: true
+  });
+}
